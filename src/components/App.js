f22367c839ec00f48f01a7f1dfda4157dfa746d8
Kamil Duliniec <123dulko@wp.pl>
2021-05-24 21:27:50 +0200

    style main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import spock from "../assets/bg/rockPaper-bg.png";
+import simpsons from "../assets/bg/simpsons-bg.png";
+import quiz from "../assets/bg/quiz-bg.png";
 
 function App() {
   return (
-    <>
-      <div>
-        <Link to="/rock">Rock Paper Scissors</Link>
+    <div className="app">
+      <div className="app__container">
+        <Link to="/rock" className="app__rock app__all">
+          <img src={spock} style={{ width: "100%" }} />
+        </Link>
+        <Link to="/memory" className="app__memory app__all">
+          <img src={simpsons} style={{ width: "100%" }} />
+        </Link>
+        <Link to="/quiz" className="app__quiz app__all">
+          <img src={quiz} style={{ width: "100%" }} />
+        </Link>
       </div>
-
-      <div>
-        <Link to="/memory">Memory Game</Link>
-      </div>
-
-      <div>
-        <Link to="/quiz">Quiz</Link>
-      </div>
-    </>
+    </div>
   );
 }
 
